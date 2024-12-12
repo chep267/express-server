48e5bcf48fe29e8ea4b1c0ccb764b4b0ead19b2b
dongntd <dong.nguyenthanh@powergatesoftware.com>
2024-12-12 17:25:03 +0700

    dongntd: update

--- src/app.ts
+++ src/app.ts
@@ -1,67 +1,12 @@
-/**
- *
- * @author dongntd267@gmail.com on 26/07/2023.
- *
- */
-
-/** libs */
-import express from 'express';
-import https from 'https';
-import fs from 'fs';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import logger from 'morgan';
-
-/** constant */
-import { AppEnv } from '@constant/AppEnv';
-
-/** configs */
-import { mongoose as chepDB, dbConfig } from '@config/database';
-
-/** routes */
-import { baseRouter } from '@route/base.route';
-import { authRouter } from '@route/auth.route';
-import { testRouter } from '@route/test.route';
-
-/** utils */
-import { connected } from '@util/log.mts';
+import express, { Request, Response } from 'express';
 
 const app = express();
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin || AppEnv.appWhiteList.split(';').includes(origin)) {
-                callback(null, true);
-            } else {
-                console.log('origin:', origin, 'not allowed');
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
-        credentials: true
-    })
-);
-// app.use(cors({ origin: `https://${clientHost}:${clientVuePort}`, credentials: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(logger('dev'));
-app.use(baseRouter);
-app.use(authRouter);
-app.use(testRouter);
-app.use((_req, res) => {
-    res.status(404).json('This api not found!');
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
 });
 
-https
-    .createServer(
-        {
-            key: fs.readFileSync('./src/utils/server.key'),
-            cert: fs.readFileSync('./src/utils/server.cert')
-        },
-        app as never
-    )
-    .listen(AppEnv.appPort, () => {
-        console.log(connected(`chep-server https start in port: ${AppEnv.appPort}`));
-    });
-
-chepDB.connect(dbConfig.uri, { dbName: dbConfig.name, bufferCommands: false }).then();
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
