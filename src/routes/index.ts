edc3780150c257a7da55536cead33b81056ad4fb
dongntd <dong.nguyenthanh@powergatesoftware.com>
2025-12-16 14:41:56 +0700

    dongntd: update express

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,14 +8,26 @@
 import { Router } from 'express';
 import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 
+/** constants */
+import { AuthApiPath } from '@constants/AuthApiPath';
+
 /** routes */
 import { authRouter } from '@routes/auth.route';
 import { testRouter } from '@routes/test.route';
 
 export const rootRouter = Router();
 
+/** home */
 rootRouter.get('/', (_req, res) => res.status(StatusCodes.OK).json(ReasonPhrases.OK));
+
+/** favicon */
 rootRouter.get('/favicon.ico', (_req, res) => res.status(StatusCodes.NO_CONTENT).end());
-rootRouter.use(authRouter);
-rootRouter.use(testRouter);
+
+/** auth */
+rootRouter.use(AuthApiPath.root, authRouter);
+
+/** test */
+rootRouter.use('/test', testRouter);
+
+/** not found */
 rootRouter.use((_req, res) => res.status(StatusCodes.NOT_FOUND).json(ReasonPhrases.NOT_FOUND));
